bda61452431d40f3dbe7dfe7e5654da476139ec5
Stefan Matei <nonsalant@gmail.com>
2026-01-08 15:40:49 +0200

    Refactor location handlers to separate module
    Moved default location event handlers from location-input.js to a new defaultLocationHandlers.js file. Updated dynamic import logic to load handlers from the new module if not implemented, improving modularity and maintainability.

--- components/location-input/location-input.js
+++ components/location-input/location-input.js
@@ -4,31 +4,7 @@ const COMPONENT_PATH = import.meta.resolve('./');
 const { Base, getHtml, } = await import(`../base/base.js?path=${encodeURIComponent(COMPONENT_PATH)}`);
 
 import { MarkerDataEvent, getAddressFromCoordinates } from '../map-picker/utils.js';
-
-// Default location event handlers 
-// can be overridden via Object.assign(LocationInput.prototype, locationHandlers);
-// before defining the element with customElements.define('location-input', LocationInput);
-// note: import the class like this so it's not auto-defined: import LocationInput from './components/location-input/location-input.js?define=false';
-const defaultLocationHandlers = {
-
-    // Log + inject coordinates + address when 'location-confirm' custom event is fired
-    handleLocationConfirm(props) {
-        console.log(`[${props.lat}, ${props.lng}]: ${props.address}`);
-        const outputEl = this.domRoot.querySelector('output');
-        outputEl.innerHTML = `<ul>
-            <li>Latitude: ${props.lat}</li>
-            <li>Longitude: ${props.lng}</li>
-            <li>Address: ${props.address}</li>
-        </ul>`;
-    },
-
-    // Clear the <output> element when the 'location-reset' custom event is fired
-    handleLocationReset() {
-        const outputEl = this.domRoot.querySelector('output');
-        outputEl.innerText = '';
-    }
-
-}
+// import { defaultLocationHandlers } from './defaultLocationHandlers.js';
 
 export default class LocationInput extends Base {
     static styles = ['critical.css',];
@@ -43,7 +19,12 @@ export default class LocationInput extends Base {
         const needsImplementation =
             typeof this.handleLocationConfirm === 'undefined' ||
             typeof this.handleLocationReset === 'undefined';
-        if (needsImplementation) Object.assign(this, defaultLocationHandlers);
+        
+        if (needsImplementation) {
+            import('./defaultLocationHandlers.js').then(defaultHandlers => {
+                Object.assign(this, defaultHandlers);
+            });
+        }
 
         this.addEventListener('location-confirm', async (e) => {
             this.setAttribute('has-location', '');
