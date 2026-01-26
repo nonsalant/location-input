8e4f6595c6c41ede382e7acc856da99c1b0ed8b8
Stefan Matei <nonsalant@gmail.com>
2026-01-26 22:36:27 +0200

    Switch lazy-modal to global CSS and update import
    Replaces the scoped CSS file with a new global lazy-modal.css and updates the JavaScript to import the new stylesheet

--- components/lazy-modal/lazy-modal.js
+++ components/lazy-modal/lazy-modal.js
@@ -13,7 +13,7 @@ const { Base, getHtml, } = await import(`../base/base.js?path=${encodeURICompone
 
 export default class LazyModal extends Base {
     static styles = [
-        'lazy-modal.scoped.css',
+        'lazy-modal.css',
         'aria-busy.css',
         // `h1 { text-decoration: underline; }`,
     ];
